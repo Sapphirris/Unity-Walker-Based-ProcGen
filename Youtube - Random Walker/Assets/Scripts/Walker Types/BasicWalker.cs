1c95a5b9395285b742173bea4f4563d1752d4a6e
Sapphirris <74716499+Sapphirris@users.noreply.github.com>
2024-02-28 12:38:17 +1030

    draft code typefied walker idea

---
+++ Youtube - Random Walker/Assets/Scripts/Walker Types/BasicWalker.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicWalker : MonoBehaviour
+{
+    private Vector2Int gridPosition;
+    private Vector2 facingDirection;
+    private Vector2Int lastValidGridPosition;
+
+    public void MoveToNextPosition()
+    {
+        
+    }
+
+    public void ReturnToLastValidPosition()
+    {
+        
+    }
+}
